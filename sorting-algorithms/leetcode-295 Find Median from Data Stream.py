85e5b7857600b016bec6502ea1ecf5bc1243cb19
sathiyaprabha012 <sathiyaprabha012@gmail.com>
2025-06-12 23:02:43 +0530

    sorting algorithms completed

--- sorting-algorithms/leetcode-295 Find Median from Data Stream.py
+++ sorting-algorithms/leetcode-295 Find Median from Data Stream.py
@@ -27,3 +27,46 @@ medianFinder.addNum(3);    // arr[1, 2, 3]
 medianFinder.findMedian(); // return 2.0
 
 '''
+
+import heapq
+class MedianFinder(object):
+
+    def __init__(self):
+        self.small = [] # stores smaller elements - follows max heap  
+        self.large = [] # stores larger elements - follows min heap 
+
+    def addNum(self, num):
+        # since python doesn't have build in max heap ,
+        # have all the elements in negative as a minheap for small
+        heapq.heappush( self.small , num * -1)
+
+        #every element in small should be lesser than every element in large
+        if self.large and self.small[0] * -1  > self.large[0] :
+            # pop from the small and add it to large
+            val = heapq.heappop(self.small) * -1 
+            heapq.heappush( self.large , val)
+
+        # check if uneven size 
+        if len(self.small) > len(self.large) + 1 :
+            # pop from the small and add it to large
+            val = heapq.heappop(self.small) * -1 
+            heapq.heappush( self.large , val)
+        
+        if len(self.large) > len(self.small) + 1 :
+            # pop from the large and add it to small
+            val = heapq.heappop(self.large)  
+            heapq.heappush( self.small , val * -1)
+
+    def findMedian(self):
+        if len(self.small) > len(self.large) :
+            return self.small[0] * -1
+        if len(self.large) > len(self.small) :
+            return self.large[0]
+        return ( self.small[0] * -1 + self.large[0] ) / 2.0
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
